45112a56790fade27a9f7b75d3a66e63be905771
Alexander <ngoxuanloc.96@gmail.com>
2017-12-06 23:56:47 +0700

    Enhance

--- WindowsFormsApplication1/WindowsFormsApplication1/FormTuyenSinh.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/FormTuyenSinh.cs
@@ -47,5 +47,34 @@ namespace WindowsFormsApplication1
         {
             ketnoicsdl(id);
         }
+
+        private void buttonXoa_Click(object sender, EventArgs e)
+        {
+            string del = dataGridViewTS.CurrentRow.Cells[0].Value.ToString();
+            con.Open();
+            string sql = "select Truong.TenTruong, TuyenSinh.* from Truong,TuyenSinh where TuyenSinh.MaTruong='" + id + "' AND Truong.MaTruong='" + id + "'";  // lay het du lieu trong bang tuyen sinh và truong
+            SqlCommand comm = new SqlCommand(sql, con); //bat dau truy van
+            con.Close();
+        }
+
+        private void comboBoxNam_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string nam = comboBoxNam.GetItemText(comboBoxNam.SelectedItem);
+            if (nam != "")
+            {      
+                con.Open();
+                string sql = "select Truong.TenTruong, TuyenSinh.* from Truong,TuyenSinh where TuyenSinh.Nam='" + nam + "' AND Truong.MaTruong='" + id + "' AND TuyenSinh.MaTruong='" + id + "'";  // lay het du lieu trong bang tuyen sinh và truong
+                SqlCommand comm = new SqlCommand(sql, con); //bat dau truy van
+                comm.CommandType = CommandType.Text;
+                SqlDataAdapter da = new SqlDataAdapter(comm); //chuyen du lieu ve
+                DataTable dt = new DataTable(); //tạo một kho ảo để lưu trữ dữ liệu
+                da.Fill(dt);  // đổ dữ liệu vào kho
+                con.Close();  // đóng kết nối
+                dataGridViewTS.DataSource = dt; //đổ dữ liệu vào datagridview
+            }
+            else {
+                ketnoicsdl(id);
+            }
+        }
     }
 }
